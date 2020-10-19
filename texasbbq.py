d3a374c86e41bf3be3459694d6ce2432d4cc5a40
Valentin Haenel <esc@users.noreply.github.com>
2020-10-19 18:20:18 +0200

    Update texasbbq.py
    Co-authored-by: stuartarchibald <stuartarchibald@users.noreply.github.com>

--- texasbbq.py
+++ texasbbq.py
@@ -438,8 +438,8 @@ def print_environment_details(target):
 
 
 def print_package_details(source_name, target_name):
-    lines= execute("conda list -n {}".format(target_name),
-                   capture=True).decode("utf-8").split("\n")
+    lines = execute("conda list -n {}".format(target_name),
+                    capture=True).decode("utf-8").split("\n")
     for l in lines[:3]:
         print(l)
     for l in lines[3:]:
