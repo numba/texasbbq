1cef9578c2150c31a4b743739886ae225240b6ee
Valentin Haenel <vhaenel@anaconda.com>
2021-01-15 13:16:25 +0100

    and scikit-image too
    As title.

--- system_test.py
+++ system_test.py
@@ -45,7 +45,7 @@ class UmapTests(GitTarget):
     @property
     def conda_dependencies(self):
         return ["numpy pytest nose scikit-learn pynndescent scipy pandas bokeh "
-                "matplotlib datashader holoviews tensorflow"]
+                "matplotlib datashader holoviews tensorflow scikit-image"]
 
     @property
     def install_command(self):
